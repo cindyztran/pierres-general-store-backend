31d9c30bb02fc8a7afa677da5c7734537bb81bf0
cindyztran <cindyztran@gmail.com>
2021-09-29 14:58:11 -0700

    adds database connection

--- server.js
+++ server.js
@@ -2,12 +2,27 @@
 
 require('dotenv').config();
 
-const {PORT = 4000} = process.env;
+const {PORT = 4000, MONGODB_URI} = process.env;
 
 const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+// Database Connection
+
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+
+});
+
+// Connection Events
+mongoose.connection
+    .on('open', () => console.log('You are connected to mongoose'))
+    .on('close', () => console.log('You are disconnected from mongoose'))
+    .on('error', () => console.log(error));
 // Routes
 app.get('/', (req, res) => {
     res.send('hello world');
